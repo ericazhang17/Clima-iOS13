b67438ab0138fa0e364262019d190161ba64d99b
ericazhang17 <zzhang4@wellesley.edu>
2020-08-10 13:41:21 -0400

    Supports dark mode, OpenWeather API, core location to fetch current location

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -7,18 +7,93 @@
 //
 
 import UIKit
+import CoreLocation
 
-class WeatherViewController: UIViewController {
-
+class WeatherViewController: UIViewController{
+    
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    var weatherManager = WeatherManager()
+    let locationManger = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManger.delegate = self
+        searchTextField.delegate = self
+        weatherManager.delegate = self
+        locationManger.requestWhenInUseAuthorization()
+        locationManger.requestLocation()
     }
+    
+    @IBAction func locationButtonPressed(_ sender: UIButton) {
+        locationManger.requestLocation()
+    }
+}
 
+//MARK: - UITextFieldDelegate
+extension WeatherViewController : UITextFieldDelegate {
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type city name"
+            return false
+        }
+    }
+        
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let city = searchTextField.text {
+            weatherManager.fetchWeather(cityName: city)
+        }
+        searchTextField.text = ""
+    }
+}
+
+//MARK: - WeatherManagerDelegate
+extension WeatherViewController:WeatherManagerDelegate {
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+    
+    
+    func didUpdateWeather(_ weatherManager:WeatherManager, weather: WeatherModel) {
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.tempStr
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+            self.cityLabel.text = weather.cityName
+        }
+        
+    }
+}
+
+//MARK: - CLLocationManagerDelegate
+extension WeatherViewController:CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationManger.stopUpdatingLocation()
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            weatherManager.fetchWeather(latitude:lat, longitute:lon)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
+    
 
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 Developed for learning purposes
+
+Purpose: Weather app that shows weather on location searched and weather in current location, supports dark mode
+
+Usage: 
+1. Request API key from OpenWeather and substitute key in WeatherManager.swift
+2. Xcode simulator -> features -> locations -> custom location -> input current longitude and latitute (works on actual phone)
+
+
+
+
